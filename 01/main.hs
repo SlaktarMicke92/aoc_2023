eade7658d772ce334f3c18001b9fe577a06550ef
CherryBoy <cherryboy@pop-os.localdomain>
2023-12-03 20:09:08 +0100

    feat: 01 Part 1 Solution

---
+++ 01/main.hs
@@ -0,0 +1,30 @@
+import Data.Char (isDigit)
+
+readData :: FilePath -> IO String
+readData file = do
+  readFile file
+
+headDef :: String -> Int
+headDef [] = 0
+headDef (x:_) = read [x]
+
+getNumber :: String -> Int
+getNumber line = headDef $ filter isDigit line
+
+getSum :: String -> Int
+getSum content
+  = sum
+  $ map processLine
+  $ lines content
+  where
+    processLine :: String -> Int
+    processLine line = do
+      let firstNumber = getNumber line
+      let secondNumber = getNumber $ reverse line
+      (firstNumber * 10) + secondNumber
+
+main :: IO ()
+main = do
+  content <- readData "input.txt"
+  let outputData = getSum content
+  print outputData
---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2023
+>Learning Haskell
+
+Me, at the beginning of the year 2023: "This year, I will start learning Haskell.".
+
+Thus learning Haskell begun.
